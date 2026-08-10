35299705e346050e9b35c942323a8bec71ee5554
Alex Khizhnyi <mendler88@gmail.com>
2026-08-10 16:25:53 +0300

    fix(jest): make the test suite runnable

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,17 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '<rootDir>/.docz/'],
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                // ts-jest forces `module: commonjs`, which is incompatible with the
+                // `moduleResolution: bundler` / `resolvePackageJsonExports` pair in tsconfig.json.
+                tsconfig: {
+                    module: 'node16',
+                    moduleResolution: 'node16',
+                },
+            },
+        ],
+    },
 };
